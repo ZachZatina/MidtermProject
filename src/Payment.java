321f65342321c26c7ce6bc050fe0277dd1919e2a
ZachZatina <zachzatina327@yahoo.com>
2018-02-07 15:59:48 -0500

    added payment and cashPayment classes

---
+++ src/Payment.java
@@ -0,0 +1,36 @@
+
+public abstract class Payment {
+	
+	private double subtotal = 0.00;
+	private double tax;
+	private double total;
+	
+	public abstract void price();
+	
+	public Payment() {
+		
+	}
+	
+	public double getSubtotal(int quantity, double price) {
+		double subTotal = 0;
+		subTotal = subtotal + (price * quantity);
+		this.subtotal = subTotal;
+		return subTotal;
+	}
+	
+	public double getTax() {
+		final double TAXRATE = 0.06;
+		double tax;
+		tax = subtotal * TAXRATE;
+		this.tax = tax;
+		return tax;	
+	}
+	
+	public double getTotal() {
+		double total;
+		total = subtotal + tax;
+		this.total = total;
+		return total;
+	}
+
+}
