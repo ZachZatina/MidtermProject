64b8505011b7b8f659366d8d7c50e745c9c22989
ZachZatina <zachzatina327@yahoo.com>
2018-02-07 16:13:40 -0500

    added CheckPayment

---
+++ src/CheckPayment.java
@@ -0,0 +1,20 @@
+
+public class CheckPayment extends Payment{
+	
+	private String checkNum;
+
+	@Override
+	public void payment() {		
+	}
+
+	public String getCheckNum() {
+		return checkNum;
+	}
+
+	public void setCheckNum(String checkNum) {
+		this.checkNum = checkNum;
+	}
+	
+	
+	
+}
