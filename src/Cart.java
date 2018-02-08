e3ae16bf2ff4cda58599da509a6250f97de36362
ZachZatina <zachzatina327@yahoo.com>
2018-02-08 10:30:50 -0500

    added cart class

---
+++ src/Cart.java
@@ -0,0 +1,43 @@
+
+public class Cart {
+	
+	private int quantity;
+	private String name;
+	private double lineTotal;
+	
+	public Cart(int quantity, String name, double lineTotal) {
+		this.quantity = quantity;
+		this.name = name;
+		this.lineTotal = lineTotal;
+		
+		
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getLineTotal() {
+		return lineTotal;
+	}
+
+	public void setLineTotal(double lineTotal) {
+		this.lineTotal = lineTotal;
+	}
+	
+	public String toString() {
+		return String.format("%1$-4d %2$-20s $%3$-6.2f \n", quantity, name, lineTotal);
+	}
+}
