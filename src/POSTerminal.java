a353466b55e3322c2b4e48156c191e8eed0d4778
ZachZatina <zachzatina327@yahoo.com>
2018-02-08 13:53:43 -0500

    Added cart class, added payment selection in main

--- src/POSTerminal.java
+++ src/POSTerminal.java
@@ -18,7 +18,8 @@ public class POSTerminal {
 		Scanner scan = new Scanner(System.in);
 
 		// prompt user: begin transaction?, [maybe: examine old transaction]
-		System.out.println("Welcome to the [name] POS Terminal! \n ---> Options (1) Begin transaction (2) Examine transactions");
+		System.out.println(
+				"Welcome to the [name] POS Terminal! \n ---> Options (1) Begin transaction (2) Examine transactions");
 		int userChoice = scan.nextInt();
 
 		ArrayList<Product> productList;
@@ -34,14 +35,14 @@ public class POSTerminal {
 			for (Product e : productList) {
 				// System.out.println(e.getProductName() + ", " + e.getProductCat() + ", " +
 				// e.getProductDesc() + ", " + e.getPrice());
-				System.out.printf("%s. %s   /   %s   /   %s   /   $%s\n", i, e.getProductName(), e.getProductCat(), e.getProductDesc(),
-						e.getPrice());
+				System.out.printf("%s. %s   /   %s   /   %s   /   $%s\n", i, e.getProductName(), e.getProductCat(),
+						e.getProductDesc(), e.getPrice());
 				i++;
 
 			}
 			// prompt: choose item? [bonus options: add an item? remove an item?]
 			System.out.println("Enter item number to add to order.");
-			int itemChoice = scan.nextInt()-1;
+			int itemChoice = scan.nextInt() - 1;
 			// Display choice and price
 			System.out.print("Enter quantity: ");
 			int itemQuantity = scan.nextInt();
@@ -49,15 +50,34 @@ public class POSTerminal {
 
 			// display: line total (current item price * quantity) -- use method
 			// add to cart
-			
-			System.out.printf("%1$-4d %2$-20s $%3$-6.2f \n", (itemChoice + 1), productList.get(itemChoice).getProductName(),
+
+			System.out.printf("%1$-4d %2$-20s $%3$-6.2f \n", itemQuantity, productList.get(itemChoice).getProductName(),
 					lineTotal);
-			//
 
-			
+			cartList = convertToCart(itemQuantity, productList.get(itemChoice), lineTotal);
+
+			scan.nextLine();// may not need this here to clear the scanner
+			String payType;
+			boolean correctType = false;
+			while (correctType == false) {
+				System.out.print("How is the customer paying? (Cash, CC (CreditCard), Check): ");
+				payType = scan.nextLine();
+
+				if (payType.equalsIgnoreCase("CASH")) {
+					correctType = true;
+					Payment p = new CashPayment(); // type the needed inputs here for the cashPayment class
+				} else if (payType.equalsIgnoreCase("CHECK")) {
+					correctType = true;
+					Payment p = new CreditCardPayment(); // put corresponding inputs in ().
+				} else if (payType.equalsIgnoreCase("CC")) {
+					correctType = true;
+					Payment p = new CheckPayment(); // type corresponding inputs.
+				} else {
+					System.out.println("This is not a valid input");
+				}
+			}
+
 		} // end if == 1
-
-
 
 		// prompt: view cart? complete order? add another item? remove item?
 
@@ -65,9 +85,9 @@ public class POSTerminal {
 
 	// method to create receipt as txt file
 	public static void createFile(String dirString, String fileString) {
-		
+
 		Path filePath = Paths.get(dirString, fileString);
-		
+
 		if (Files.notExists(filePath)) {
 			try {
 				Files.createFile(filePath);
@@ -78,7 +98,7 @@ public class POSTerminal {
 			}
 		}
 	}
-	
+
 	// method to write to receipt file
 	// name, phone, email
 	// use toString from Cart
@@ -86,8 +106,7 @@ public class POSTerminal {
 	// subtotal
 	// tax
 	// total
-	
-	
+
 	public static void writeReceipt(String dirString, String filePath, Cart cart) {
 
 		Path writeFile = Paths.get(dirString, filePath);
@@ -105,7 +124,6 @@ public class POSTerminal {
 			e.printStackTrace();
 		}
 	}
-
 
 	// method to create product list as an ArrayList, reading from txt file
 	public static ArrayList<Product> createProductList() {
@@ -125,7 +143,7 @@ public class POSTerminal {
 				// splits the line up on commas, then adds them to an array
 				// then uses the pieces from the array to create a new object and add
 				// that to productArrayList
-				String singleProductArray[] = line.split(",");
+				String singleProductArray[] = line.split("-");
 				productArrayList.add(new Product(singleProductArray[0], singleProductArray[1], singleProductArray[2],
 						Double.parseDouble(singleProductArray[3])));
 
@@ -143,9 +161,18 @@ public class POSTerminal {
 		return productArrayList;
 
 	}
-	
+
 	public static double LineTotal(double price, int quantity) {
 		return price * quantity;
 	}
-	
+
+	public static ArrayList<Cart> convertToCart(int quantity, Product product, double lineTotal) {
+
+		ArrayList<Cart> cart = new ArrayList<>();
+		String name = product.getProductName();
+		Cart input = new Cart(quantity, name, lineTotal);
+		cart.add(input);
+		return cart;
+
+	}
 }
