134e70e04cb4d08edebb862901ae24f98b36dbe8
ZachZatina <zachzatina327@yahoo.com>
2018-02-08 10:23:16 -0500

    saved changes that didnt get pushed before

--- src/POSTerminal.java
+++ src/POSTerminal.java
@@ -8,65 +8,58 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class POSTerminal {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
-		
+
 		// prompt user: begin transaction?, [maybe: examine old transaction]
 		System.out.println("Welcome to the POS Terminal. (1) Begin transaction (2) Examine transactions");
 		int userChoice = scan.nextInt();
-	
+
 		ArrayList<Product> productList;
-		
+
 		if (userChoice == 1) {
 			// if no, exit; if yes: Display menu
 			productList = new ArrayList<Product>();
 			productList = createProductList();
 			System.out.println("Menu: (Item, Category, Description, Price)\n");
-			
-			int i = 1; 
+
+			int i = 1;
 			for (Product e : productList) {
-				//System.out.println(e.getProductName() + ", " + e.getProductCat() + ", " + e.getProductDesc() + ", " + e.getPrice());
+				// System.out.println(e.getProductName() + ", " + e.getProductCat() + ", " +
+				// e.getProductDesc() + ", " + e.getPrice());
 				System.out.printf("%s. %s, %s, %s, $%s\n", i, e.getProductName(), e.getProductCat(), e.getProductDesc(),
 						e.getPrice());
 				i++;
-			
-			} 
+
+			}
 			// prompt: choose item? [bonus options: add an item? remove an item?]
-			System.out.println("Select an item to purchase. Enter item number."); 
-			int itemChoice = scan.nextInt() -1;
+			System.out.println("Select an item to purchase. Enter item number.");
+			int itemChoice = scan.nextInt() - 1;
 			// Display choice and price
-			System.out.printf("%s, $%s", productList.get(itemChoice).getProductName(), productList.get(itemChoice).getPrice());
-				
+			System.out.println("Enter quantity:");
+			int itemQuantity = scan.nextInt();
+
+			// display: line total (current item price * quantity) -- use method
+			// add to cart
+			
+			System.out.printf("%s, $%s", productList.get(itemChoice).getProductName(),
+					productList.get(itemChoice).getPrice());
+			//
+
 			
 		} // end if == 1
-		
-		
-		
-		// display item and price
-		
-		
-		// prompt: enter the quantity
-		
-		// display: line total (current item price * quantity)
-		// add to cart
-		
-		
-		// prompt: view cart? complete order? add another item? remove item? 
-		
-		
-		
-		
-		
-		
-		
+
+
+
+		// prompt: view cart? complete order? add another item? remove item?
+
 	}
 
 	public static ArrayList<Product> createProductList() {
-		
+
 		ArrayList<Product> productArrayList = new ArrayList<Product>();
 
 		Path readFile = Paths.get("productList.txt");
@@ -76,20 +69,20 @@ public class POSTerminal {
 			FileReader fr = new FileReader(file);
 			BufferedReader reader = new BufferedReader(fr);
 			String line = reader.readLine();
-			
+
 			while (line != null) {
 				// System.out.println(line);
 				// splits the line up on commas, then adds them to an array
 				// then uses the pieces from the array to create a new object and add
 				// that to productArrayList
 				String singleProductArray[] = line.split(",");
-				productArrayList.add(new Product(singleProductArray[0],singleProductArray[1],singleProductArray[2],Double.parseDouble(
-						singleProductArray[3])));
-						
+				productArrayList.add(new Product(singleProductArray[0], singleProductArray[1], singleProductArray[2],
+						Double.parseDouble(singleProductArray[3])));
+
 				line = reader.readLine();
 			}
 			reader.close();
-			
+
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found");
 			e.printStackTrace();
@@ -98,6 +91,6 @@ public class POSTerminal {
 			e.printStackTrace();
 		}
 		return productArrayList;
-	
+
 	}
 }
